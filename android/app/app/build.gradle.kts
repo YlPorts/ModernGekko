bbe2b42958c411e3db979b252a275c9effc0fe5c
Jude <133681884+YlPorts@users.noreply.github.com>
2026-07-29 22:52:38 -0500

    android: build app against Dolphin Android core

--- android/app/app/build.gradle.kts
+++ android/app/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
 }
 
+val repositoryRoot = rootProject.projectDir.parentFile.parentFile
+
 android {
     namespace = "org.moderngekko.android"
     compileSdk = 36
@@ -21,9 +23,21 @@ android {
         externalNativeBuild {
             cmake {
                 arguments += listOf(
-                    "-DANDROID_STL=c++_shared",
+                    "-DANDROID_STL=c++_static",
                     "-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON",
-                    "-DMODERNGEKKO_ANDROID_WITH_DOLPHIN=ON"
+                    "-DMODERNGEKKO_ROOT=${repositoryRoot.absolutePath}",
+                    "-DCMAKE_PROJECT_INCLUDE=${repositoryRoot.absolutePath}/android/native/dolphin_inject.cmake",
+                    "-DUSE_SYSTEM_LIBS=OFF",
+                    "-DENABLE_QT=OFF",
+                    "-DENABLE_TESTS=OFF",
+                    "-DENABLE_ANALYTICS=OFF",
+                    "-DENABLE_AUTOUPDATE=OFF",
+                    "-DENABLE_LLVM=OFF",
+                    "-DENCODE_FRAMEDUMPS=OFF",
+                    "-DUSE_RETRO_ACHIEVEMENTS=OFF",
+                    "-DUSE_DISCORD_PRESENCE=OFF",
+                    "-DUSE_MGBA=OFF",
+                    "-DUSE_UPNP=OFF"
                 )
                 targets += listOf("moderngekko_android")
             }
@@ -38,8 +52,14 @@ android {
 
     externalNativeBuild {
         cmake {
-            path = file("../../native/CMakeLists.txt")
+            path = file("../../../vendor/dolphin/CMakeLists.txt")
             version = "3.22.1+"
+        }
+    }
+
+    sourceSets {
+        getByName("main") {
+            assets.srcDir(file("../../../vendor/dolphin/Data"))
         }
     }
 
